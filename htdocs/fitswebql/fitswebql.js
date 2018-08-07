1a207c16e67a0c823154bf46f87b6df4358d3905
jvo203 <chris.zapart@nao.ac.jp>
2018-08-07 14:42:56 +0900

    try to delete the welcome screen

--- htdocs/fitswebql/fitswebql.js
+++ htdocs/fitswebql/fitswebql.js
@@ -7853,7 +7853,7 @@ function fetch_spectrum(datasetId, index, add_timestamp)
 	
 	if (xmlhttp.readyState == 4 && xmlhttp.status == 200)
 	{
-	    //document.getElementById('welcome').style.display = "none";	    
+	    document.getElementById('welcome').style.display = "none";	    
 	    
 	    //console.log(xmlhttp.responseText) ;
 	    
