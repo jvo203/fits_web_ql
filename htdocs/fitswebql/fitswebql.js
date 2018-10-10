8d5816b5ff9dc832edbfee4eb64b6e8be22e788c
jvo203 <chris.zapart@nao.ac.jp>
2018-10-10 18:30:13 +0900

    testing partial downloads

--- htdocs/fitswebql/fitswebql.js
+++ htdocs/fitswebql/fitswebql.js
@@ -7765,8 +7765,8 @@ function partial_fits_download(offsetx, offsety, width, height) {
 	url += "x1=" + Math.round(orig_x1) + "&y1=" + Math.round(orig_y2) + "&x2=" + Math.round(orig_x2) + "&y2=" + Math.round(orig_y1) + "&frame_start=" + data_band_lo + "&frame_end=" + data_band_hi + "&ref_freq=" + RESTFRQ;
 
 	//console.log(url) ;
-	//window.location.assign(url);
-	window.open(url, '_blank');
+	window.location.assign(url);
+	//window.open(url, '_blank');
 }
 
 function ok_download() {
