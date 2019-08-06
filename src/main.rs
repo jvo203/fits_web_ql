c021cbf7b7517c0c26a2a47d5030616f5b1657c5
jvo203 <chris.zapart@nao.ac.jp>
2019-08-06 17:51:58 +0900

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -4237,7 +4237,7 @@ fn main() {
                 server_address = value.clone();
             }
 
-            if key == "--cluster_root_host" {
+            if key == "--cluster_root" {
                 root_node = Some(value.clone());
             }
 
