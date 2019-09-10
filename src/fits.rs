3bbb7ca30e8fa6148a75b4070e99786260a48df1
jvo203 <chris.zapart@nao.ac.jp>
2019-09-10 11:57:20 +0900

    modified:   src/fits.rs

--- src/fits.rs
+++ src/fits.rs
@@ -875,8 +875,9 @@ impl FITS {
                                 match res.text() {
                                     Ok(body) => {
                                         if let Ok((start, end)) =
-                                            scan_fmt!(&body, "{d} {d}", usize, usize)
+                                            scan_fmt!(&body, "{d}{d}", usize, usize)
                                         {
+                                            println!("start: {}, end: {}", start, end);
                                             _break_loop = false;
                                             (start, end)
                                         } else {
@@ -903,6 +904,7 @@ impl FITS {
 
                 match schedule_jobs(self.dataset_id.clone(), self.depth, &local_ip, num_threads) {
                     Some((start, end)) => {
+                        println!("start: {}, end: {}", start, end);
                         _break_loop = false;
                         (start, end)
                     }
