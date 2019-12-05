4f7041f359940dcea472f9d3092edf3f069bfac9
jvo203 <chris.zapart@nao.ac.jp>
2019-12-05 10:22:41 +0900

    a version upgrade

--- src/main.rs
+++ src/main.rs
@@ -2371,7 +2371,7 @@ lazy_static! {
 #[cfg(feature = "jvo")]
 static LOG_DIRECTORY: &'static str = "LOGS";
 
-static SERVER_STRING: &'static str = "FITSWebQL v4.1.24";
+static SERVER_STRING: &'static str = "FITSWebQL v4.1.25";
 static VERSION_STRING: &'static str = "SV2019-12-05.0";
 static WASM_STRING: &'static str = "WASM2019-02-08.1";
 
