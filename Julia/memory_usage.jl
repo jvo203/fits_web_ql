aeca8a8b00237ad58a89972c3246a18d6e6f74dd
ザパート・クリストファー <chris.zapart@nao.ac.jp>
2020-08-13 10:07:11 +0900

    modified:   Julia/memory_usage.jl

--- Julia/memory_usage.jl
+++ Julia/memory_usage.jl
@@ -7,5 +7,5 @@ println(data[1:5,:])
 timestamp = data[:,1] ./ 1000
 allocated = data[:,2] ./ (1024 * 1024)
 
-plot(timestamp, allocated, label="jemalloc stats.allocated memory usage [MB]", xlabel="elapsed time [s]", ylabel="memory [MB]")
+plot(timestamp, allocated, label="jemalloc stats.allocated memory [MB]", xlabel="elapsed time [s]", ylabel="memory [MB]")
 savefig("mem_allocated_v4.pdf")
