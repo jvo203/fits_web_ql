6b016534c0687bd6eace90992fe2c77b42387b19
jvo203 <chris.zapart@nao.ac.jp>
2020-11-29 17:05:45 +0900

    added a new function fn vpx_codec_enc_config_init() -> vpx_codec_enc_cfg_t

--- src/main.rs
+++ src/main.rs
@@ -189,6 +189,65 @@ pub struct UserParams {
     pixels: Vec<f32>,
 }
 
+fn vpx_codec_enc_config_init() -> vpx_codec_enc_cfg_t {
+    vpx_codec_enc_cfg_t {
+        g_usage: 0,
+        g_threads: 0,
+        g_profile: 0,
+        g_w: 0,
+        g_h: 0,
+        g_bit_depth: vpx_bit_depth::VPX_BITS_8,
+        g_input_bit_depth: 8,
+        g_timebase: vpx_rational {
+            num: 0,
+            den: 0,
+        },
+        g_error_resilient: 0,
+        g_pass: vpx_enc_pass::VPX_RC_ONE_PASS,
+        g_lag_in_frames: 0,
+        rc_dropframe_thresh: 0,
+        rc_resize_allowed: 0,
+        rc_scaled_width: 0,
+        rc_scaled_height: 0,
+        rc_resize_up_thresh: 0,
+        rc_resize_down_thresh: 0,
+        rc_end_usage: vpx_rc_mode::VPX_VBR, 
+        rc_twopass_stats_in: vpx_fixed_buf {
+            buf: ptr::null_mut(),
+            sz: 0,
+        },
+        rc_firstpass_mb_stats_in: vpx_fixed_buf {
+            buf: ptr::null_mut(),
+            sz: 0,
+        },
+        rc_target_bitrate: 0,
+        rc_min_quantizer: 0,
+        rc_max_quantizer: 0,
+        rc_undershoot_pct: 0,
+        rc_overshoot_pct: 0,
+        rc_buf_sz: 0,
+        rc_buf_initial_sz: 0,
+        rc_buf_optimal_sz: 0,
+        rc_2pass_vbr_bias_pct: 0,
+        rc_2pass_vbr_minsection_pct: 0,
+        rc_2pass_vbr_maxsection_pct: 0,
+        kf_mode: vpx_kf_mode::VPX_KF_AUTO,
+        kf_min_dist: 0,
+        kf_max_dist: 0,
+        ss_number_layers: 0,
+        ss_enable_auto_alt_ref: [0; 5],
+        ss_target_bitrate: [0; 5],
+        ts_number_layers: 0,
+        ts_target_bitrate: [0; 5],
+        ts_rate_decimator: [0; 5],
+        ts_periodicity: 0,
+        ts_layer_id: [0; 16],
+        layer_target_bitrate: [0; 12],
+        temporal_layering_mode: 0,
+        rc_2pass_vbr_corpus_complexity: 0,
+    }
+}
+
 struct UserSession {
     addr: Addr<server::SessionServer>,
     dataset_id: Vec<String>,
@@ -268,7 +327,8 @@ impl UserSession {
             log: log,
             wasm: false,
             //hevc: hevc,
-            cfg: vpx_codec_enc_cfg::default(),
+            //cfg: vpx_codec_enc_cfg::default(),
+            cfg: vpx_codec_enc_config_init(),
             ctx: vpx_codec_ctx_t {
                 name: ptr::null(),
                 iface: ptr::null_mut(),
@@ -1567,7 +1627,8 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for UserSession {
                                 //a workaround around a bug in libvpx triggered when h > w
                                 raw.stride[0] = if w > h { w as i32 } else { h as i32 };
 
-                                let mut cfg = vpx_codec_enc_cfg::default();
+                                //let mut cfg = vpx_codec_enc_cfg::default();
+                                let mut cfg = vpx_codec_enc_config_init();
                                 let mut ret = unsafe {
                                     vpx_codec_enc_config_default(vpx_codec_vp9_cx(), &mut cfg, 0)
                                 };
@@ -2402,8 +2463,8 @@ lazy_static! {
 #[cfg(feature = "jvo")]
 static LOG_DIRECTORY: &'static str = "LOGS";
 
-static SERVER_STRING: &'static str = "FITSWebQL v4.2.6";
-static VERSION_STRING: &'static str = "SV2020-10-27.0";
+static SERVER_STRING: &'static str = "FITSWebQL v4.2.7";
+static VERSION_STRING: &'static str = "SV2020-11-29.0";
 //static WASM_STRING: &'static str = "WASM2019-02-08.1";
 static WASM_STRING: &'static str = "WASM2020-06-22.0";
 static FPZIP_STRING: &'static str = "WASM2020-06-18.0";
