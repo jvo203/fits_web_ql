3a799d4988c4cbc5c130c77cc8d67ae8702c769b
jvo203 <chris.zapart@nao.ac.jp>
2021-05-18 20:55:10 +0900

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -2465,7 +2465,7 @@ lazy_static! {
 static LOG_DIRECTORY: &'static str = "LOGS";
 
 static SERVER_STRING: &'static str = "FITSWebQL v4.2.7";
-static VERSION_STRING: &'static str = "SV2021-02-21.0";
+static VERSION_STRING: &'static str = "SV2021-05-18.0";
 //static WASM_STRING: &'static str = "WASM2019-02-08.1";
 static WASM_STRING: &'static str = "WASM2020-06-22.0";
 static FPZIP_STRING: &'static str = "WASM2020-06-18.0";
