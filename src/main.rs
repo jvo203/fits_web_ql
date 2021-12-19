c9834b2ec9adb5937c780273b097ba86fabae23a
jvo203 <chris.zapart@nao.ac.jp>
2021-12-19 10:30:21 +0900

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -4131,7 +4131,7 @@ fn http_fits_response(
 
     // HTML5 FileSaver + Font Awesome
     html.push_str("<script src=\"https://kit.fontawesome.com/8433b7dde2.js\" crossorigin=\"anonymous\"></script>\n");
-    html.push_str("<script src=\"https://cdn.jsdelivr.net/gh/jvo203/fits_web_ql/htdocs/fitswebql/FileSaver.js\"></script>\n");
+    html.push_str("<script src=\"FileSaver.js\"></script>\n");
 
     //VP9 decoder
     #[cfg(not(feature = "cdn"))]
