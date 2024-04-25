27840bf442a9322bc8ab293cd4b6793a7609dbef
jvo203 <chris.zapart@nao.ac.jp>
2024-04-25 14:16:02 +0900

    re-compiled the FPZIP decoder with a newer emscripten compiler

--- src/main.rs
+++ src/main.rs
@@ -2675,10 +2675,10 @@ lazy_static! {
 #[cfg(feature = "jvo")]
 static LOG_DIRECTORY: &'static str = "LOGS";
 
-static SERVER_STRING: &'static str = "FITSWebQL v4.4.19";
-static VERSION_STRING: &'static str = "R/SV2024-03-18.0";
+static SERVER_STRING: &'static str = "FITSWebQL v4.4.20";
+static VERSION_STRING: &'static str = "R/SV2024-04-25.0";
 static WASM_STRING: &'static str = "WASM2020-06-22.0";
-static FPZIP_STRING: &'static str = "WASM2020-06-18.0";
+static FPZIP_STRING: &'static str = "WASM2024-04-25.0";
 
 #[cfg(not(feature = "jvo"))]
 static SERVER_MODE: &'static str = "LOCAL";
