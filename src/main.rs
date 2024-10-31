c8a576658b5880e638d49a64b07a47e3880a1c70
jvo203 <chris.zapart@nao.ac.jp>
2024-10-31 13:42:43 +0900

    a version update, fixed a compatibility with x265 v4 C library

--- src/main.rs
+++ src/main.rs
@@ -2172,6 +2172,7 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for UserSession {
 
                                     let mut nal_count: u32 = 0;
                                     let mut p_nal: *mut x265_nal = ptr::null_mut();
+                                    let mut p_out: *mut x265_picture = ptr::null_mut();
 
                                     //encode
                                     let ret = unsafe {
@@ -2180,7 +2181,7 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for UserSession {
                                             &mut p_nal,
                                             &mut nal_count,
                                             self.pic,
-                                            ptr::null_mut(),
+                                            &mut p_out,
                                         )
                                     };
 
@@ -2247,7 +2248,7 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for UserSession {
                                                 &mut p_nal,
                                                 &mut nal_count,
                                                 ptr::null_mut(),
-                                                ptr::null_mut(),
+                                                &mut p_out,
                                             )
                                         };
 
@@ -2518,6 +2519,7 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for UserSession {
 
                                 let mut nal_count: u32 = 0;
                                 let mut p_nal: *mut x265_nal = ptr::null_mut();
+                                let mut p_out: *mut x265_picture = ptr::null_mut();
 
                                 //encode
                                 let ret = unsafe {
@@ -2526,7 +2528,7 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for UserSession {
                                         &mut p_nal,
                                         &mut nal_count,
                                         self.pic,
-                                        ptr::null_mut(),
+                                        &mut p_out,
                                     )
                                 };
 
@@ -2595,7 +2597,7 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for UserSession {
                                             &mut p_nal,
                                             &mut nal_count,
                                             ptr::null_mut(),
-                                            ptr::null_mut(),
+                                            &mut p_out,
                                         )
                                     };
 
@@ -2675,8 +2677,8 @@ lazy_static! {
 #[cfg(feature = "jvo")]
 static LOG_DIRECTORY: &'static str = "LOGS";
 
-static SERVER_STRING: &'static str = "FITSWebQL v4.4.21";
-static VERSION_STRING: &'static str = "R/SV2024-09-06.0";
+static SERVER_STRING: &'static str = "FITSWebQL v4.5.0";
+static VERSION_STRING: &'static str = "R/SV2024-10-31.0";
 static WASM_STRING: &'static str = "WASM2024-09-06.0";
 static FPZIP_STRING: &'static str = "WASM2024-09-06.0";
 
