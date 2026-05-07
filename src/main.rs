d7f550671754fc88cb314bd8830c71767fb19018
jvo203 <chris.zapart@nao.ac.jp>
2026-05-07 11:03:25 +0900

    self-host FontAwesome v5

--- src/main.rs
+++ src/main.rs
@@ -2850,7 +2850,7 @@ lazy_static! {
 static LOG_DIRECTORY: &'static str = "LOGS";
 
 static SERVER_STRING: &'static str = "FITSWebQL v4.5.7";
-static VERSION_STRING: &'static str = "R/SV2026-05-01.0";
+static VERSION_STRING: &'static str = "R/SV2026-05-07.0";
 static WASM_STRING: &'static str = "WASM2025-01-20.0";
 static FPZIP_STRING: &'static str = "WASM2025-01-20.0";
 
@@ -4700,7 +4700,8 @@ fn http_fits_response(
     html.push_str("<script src=\"https://cdn.jsdelivr.net/gh/jvo203/fits_web_ql/htdocs/fitswebql/FileSaver.js\"></script>\n");
 
     // Font Awesome
-    html.push_str("<script src=\"https://kit.fontawesome.com/8433b7dde2.js?ver=5.15.4\" crossorigin=\"anonymous\"></script>\n");
+    //html.push_str("<script src=\"https://kit.fontawesome.com/8433b7dde2.js?ver=5.15.4\" crossorigin=\"anonymous\"></script>\n");
+    // local hosting
 
     //VP9 decoder
     #[cfg(not(feature = "cdn"))]
